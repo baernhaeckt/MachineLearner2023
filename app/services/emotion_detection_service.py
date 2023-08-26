430e1bf247aaba3107ce9e83a92e5e4ace89c9c1
Hofmann Oliver <oliver.hofmann@ti8m.ch>
2023-08-26 13:47:11 +0200

    Split long audio in to chunks

--- app/services/emotion_detection_service.py
+++ app/services/emotion_detection_service.py
@@ -5,7 +5,9 @@ import tensorflow as tf
 
 
 class EmotionDetectionService:
-    EMOTION_CATEGORIES = {0: "anger", 1: "anxiety", 2: "boredom", 3: "disgust", 4: "happiness", 5: "neutral", 6: "sadness"}
+    EMOTION_CATEGORIES: dict = {0: "anger", 1: "anxiety", 2: "boredom", 3: "disgust", 4: "happiness", 5: "neutral", 6: "sadness"}
+    SEGMENT_DURATION_SECS: float = 5
+
     @staticmethod
     def _extract_features(data, sample_rate):
         result = np.array([])
@@ -31,30 +33,50 @@ class EmotionDetectionService:
 
         return result
 
-    def detect_audio(self, file_path: str):
-        data, sample_rate = librosa.load(file_path, duration=2.5, offset=0.6)
+    def _predict(self, data, index, segment_length, sample_rate):
+        split_data = data[index * segment_length: (index + 1) * segment_length]
 
         feature_list = list()
-        feature = self._extract_features(data, sample_rate)
+        feature = self._extract_features(split_data, sample_rate)
         feature_list.append(feature)
 
         audio_features = pd.DataFrame(feature_list)
 
         model = tf.keras.models.load_model("emotion-detection.h5")
         predictions = model.predict(audio_features)
-
         prediction_proba = tf.nn.sigmoid(predictions)
-        predicted_class = self.EMOTION_CATEGORIES[np.argmax(predictions)]
 
-
-        category_probabilities = list()
+        all_probabilities = list()
         for i, p in enumerate(prediction_proba.numpy()[0]):
-            category_probabilities.append({
+            all_probabilities.append({
                 "class": self.EMOTION_CATEGORIES[i],
                 "probability": p * 100
             })
 
+        return pd.DataFrame(all_probabilities)
+
+    def detect_audio(self, file_path: str) -> dict:
+        data, sample_rate = librosa.load(file_path, offset=0.6)
+
+        segment_length = int(sample_rate * self.SEGMENT_DURATION_SECS)
+        number_sections = int(np.ceil(len(data) / segment_length))
+
+
+        split_proba_df = pd.DataFrame()
+        predicted_classes = list()
+
+        for i in range(number_sections):
+            predict_df = self._predict(data, i, segment_length, sample_rate)
+            split_proba_df = pd.concat([split_proba_df, predict_df])
+
+            split_class = predict_df.loc[split_proba_df.idxmax(numeric_only=True)].values[0][0]
+            predicted_classes.append(split_class)
+
+
+        class_max = split_proba_df.groupby(["class"]).max()
+
         return {
-            "predicted_class": predicted_class,
-            "probabilities": category_probabilities
+            "predicted_classes": predicted_classes,
+            "probabilities_max": [{"class": p[0], "probability": p[1]} for p in class_max["probability"].items()]
         }
+
